5303ebad6d15b6d61b1d2dbb2ddbca5a2ab6889f
irham asyhari <asyhari.irham@gmail.com>
2017-03-24 07:42:57 +0700

    buat tabel instansi

---
+++ database/migrations/2017_03_24_003841_create_table_instansi.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableInstansi extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       Schema::create('sekolah', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('nama_instansi',20);
+                $table->string('alamat_instansi');
+                $table->string('kota', 20);                
+                $table->timestamps();
+
+             }); 
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('instansi');
+    }
+}
